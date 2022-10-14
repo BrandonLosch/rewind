f233d41b407bfb4edae547cf9b05a6817f82bea4
BrandonLosch <113945271+BrandonLosch@users.noreply.github.com>
2022-10-14 14:21:43 -0400

    Challenge completed

--- js/site.js
+++ js/site.js
@@ -1,15 +1,36 @@
-function displayMessage() {
+let dc = function(id){
+    return document.getElementById(id);
+};
 
-    let msg = document.getElementById("message").value;
-    //alert(msg);
-    
-    Swal.fire(
-    {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
+//get string from the page
+//controller function
+function getValue(){
+    dc("alert").classList.add("invisible");
+
+    let userString = dc("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+//reverse the string
+//logic function
+function reverseString(userString){
+    let revString = [];
+
+    //reverse string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
     }
-
-    )
+    return revString;
 }
 
+//display alert with reversed string to the user
+//view function
+function displayString(revString){
+    //write message
+    dc("msg").innerHTML = `Your String Reversed is: ${revString}`;
+
+    //show alert
+    dc("alert").classList.remove("invisible");
+}
